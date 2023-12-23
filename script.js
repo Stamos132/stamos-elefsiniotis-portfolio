5154a3c606b6680c8224e0565de54a12df2de8d0
Stamos132 <stamos.elefsiniotis@outlook.com>
2023-12-23 15:40:43 +0200

    New formatting

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 /* Toggles the visibility of the hamburger menu */
 function toggleMenu() {
-    const menu = document.querySelector('.menu-links');
-    const icon = document.querySelector('.hamburger-icon');
-    menu.classList.toggle('open');
-    icon.classList.toggle('open');
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamburger-icon');
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
 }
 
 /* Removes the 'open' class from the hamburger menu */
 function closeMenu() {
-    const menu = document.querySelector('.menu-links');
-    const icon = document.querySelector('.hamburger-icon');
-    menu.classList.remove('open');
-    icon.classList.remove('open');
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamburger-icon');
+  menu.classList.remove('open');
+  icon.classList.remove('open');
 }
 
 /* Listens for a click event for the hamburger menu */
 var hamburgerIcon = document.querySelector('.hamburger-icon');
 
 document.addEventListener('click', function (event) {
-    const menu = document.querySelector('.menu-links');
-    if (
-        event.target === menu ||
-        menu.contains(event.target) ||
-        event.target === hamburgerIcon ||
-        hamburgerIcon.contains(event.target)
-    ) {
-        return;
-    }
-    if (menu.classList.contains('open')) {
-        closeMenu();
-    }
+  const menu = document.querySelector('.menu-links');
+  if (
+    event.target === menu ||
+    menu.contains(event.target) ||
+    event.target === hamburgerIcon ||
+    hamburgerIcon.contains(event.target)
+  ) {
+    return;
+  }
+  if (menu.classList.contains('open')) {
+    closeMenu();
+  }
 });
 
 /* Closes hamburger menu when scrolling */
 document.addEventListener('scroll', function () {
-    const menu = document.querySelector('.menu-links');
-    if (menu.classList.contains('open')) {
-        closeMenu();
-    }
+  const menu = document.querySelector('.menu-links');
+  if (menu.classList.contains('open')) {
+    closeMenu();
+  }
 });
